4c6893148143be6d9f7f052cf82413f0d2b0cb7c
Fotis Tsioumas <fotis.tsioumas@outlook.com>
2023-09-22 03:19:09 +0300

    Templates changes & Update DB Schema

--- src/main/resources/db_schema/doctor_appointment.sql
+++ src/main/resources/db_schema/doctor_appointment.sql
@@ -35,7 +35,7 @@ CREATE TABLE `appointment` (
   KEY `FKg90ck1kd0p4rbamwc22jd2oql` (`patient_id`),
   CONSTRAINT `FK1l62fhlqqc08wmgrvn7hjtfom` FOREIGN KEY (`doctor_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FKg90ck1kd0p4rbamwc22jd2oql` FOREIGN KEY (`patient_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `appointment` (
 
 LOCK TABLES `appointment` WRITE;
 /*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
-INSERT INTO `appointment` VALUES (1,'2023-09-21','12:00',0,NULL,1,NULL),(2,'2023-09-21','12:30',1,'Έλεγχος αιματολογικών εξετάσεων.',1,2),(3,'2023-09-21','13:00',0,NULL,1,NULL),(4,'2023-09-21','13:30',0,NULL,1,NULL),(5,'2023-09-21','14:00',0,NULL,1,NULL),(6,'2023-09-21','14:30',0,NULL,1,NULL),(7,'2023-09-21','15:00',1,'Πόνο στην κοιλιά.',1,3),(8,'2023-09-21','15:30',0,NULL,1,NULL),(9,'2023-09-21','16:00',0,NULL,1,NULL),(10,'2023-09-21','16:30',0,NULL,1,NULL),(11,'2023-09-21','17:00',0,NULL,1,NULL),(12,'2023-09-21','17:30',0,NULL,1,NULL),(13,'2023-09-21','18:00',0,NULL,1,NULL);
+INSERT INTO `appointment` VALUES (1,'2023-09-21','12:00',0,NULL,1,NULL),(2,'2023-09-21','12:30',1,'Έλεγχος αιματολογικών εξετάσεων.',1,2),(3,'2023-09-21','13:00',0,NULL,1,NULL),(4,'2023-09-21','13:30',0,NULL,1,NULL),(5,'2023-09-21','14:00',0,NULL,1,NULL),(6,'2023-09-21','14:30',1,'Πόνος στο στομάχι.',1,6),(7,'2023-09-21','15:00',1,'Πόνο στην κοιλιά.',1,3),(8,'2023-09-21','15:30',0,NULL,1,NULL),(9,'2023-09-21','16:00',0,NULL,1,NULL),(10,'2023-09-21','16:30',0,NULL,1,NULL),(11,'2023-09-21','17:00',0,NULL,1,NULL),(12,'2023-09-21','17:30',0,NULL,1,NULL),(13,'2023-09-21','18:00',0,NULL,1,NULL),(14,'2023-09-22','14:00',1,'Πιθανή μόλυνση στο μάτι.',4,2),(15,'2023-09-22','14:30',0,NULL,4,NULL),(16,'2023-09-22','15:00',0,NULL,4,NULL),(17,'2023-09-22','15:30',1,'Ερεθισμός στο μάτι.',4,3),(18,'2023-09-22','16:00',0,NULL,4,NULL),(19,'2023-09-22','16:30',0,NULL,4,NULL),(20,'2023-09-23','12:00',1,'Εμβόλιο ανεμοβλογίας.',5,6),(21,'2023-09-23','12:30',0,NULL,5,NULL),(22,'2023-09-23','13:00',0,NULL,5,NULL);
 /*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,7 +91,7 @@ CREATE TABLE `user` (
   `telephone` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +100,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Πλάτωνος 134, 17674 Καλλιθέα','g.papatsikhs@domain.com','Γιάννης','Παπατσίκης','$2a$10$JY2CX9WZ8TYsCszkocCD1eblzXapeQuVnsdd5Ri5UUzB82.4B0rFC','Παθολόγος','2101245789'),(2,NULL,'p.galatas@domain.com','Πέτρος','Γαλατας','$2a$10$IW6YJKwEMitfC.1BCKuu8.Q2jv3G0zRUq7yT7x0/v87MGedITArHG',NULL,'2101248963'),(3,NULL,'fotis.tsioumas@outlook.com','Φώτης','Τσιούμας','$2a$10$U8etkZbZTUwcDKkrinTKBu0IcFObQ0RN0YdK69R5qYuzqzLskbGi.',NULL,'6984470955');
+INSERT INTO `user` VALUES (1,'Πλάτωνος 134, 17674 Καλλιθέα','f.tsioumas@domain.com','Φώτης','Τσιούμας','$2a$10$JY2CX9WZ8TYsCszkocCD1eblzXapeQuVnsdd5Ri5UUzB82.4B0rFC','Παθολόγος','2101245789'),(2,NULL,'a.sideri@domain.com','Άσπα','Σιδέρη','$2a$10$JY2CX9WZ8TYsCszkocCD1eblzXapeQuVnsdd5Ri5UUzB82.4B0rFC',NULL,'2101248963'),(3,NULL,'g.papatsikhs@domain.com','Γιάννης','Παπατσίκης','$2a$10$JY2CX9WZ8TYsCszkocCD1eblzXapeQuVnsdd5Ri5UUzB82.4B0rFC',NULL,'6984470955'),(4,'Ακροπόλεως 4, 14121 Ηράκλειο','p.takanhs@domain.com','Πέτρος','Τακάνης','$2a$10$JY2CX9WZ8TYsCszkocCD1eblzXapeQuVnsdd5Ri5UUzB82.4B0rFC','Οφθαλμίατρος','6985632147'),(5,'Σωκράτους 38, 16673 Βούλα','h.panagiotou@domain.com','Ηλίας','Παναγιώτου','$2a$10$ZgaolHjsLrhkL5mgUAZHZOYpyQ10EfacY6CztHK2Q5QDBDPH2bnCm','Παιδίατρος','2104578963'),(6,NULL,'m.batakh@domain.com','Μαρία','Μπατάκη','$2a$10$NsWiNYwg7zRMXuMxYcOqWODH2yXTUc1IHg/fs9xIL56OcMTQ/nYQO',NULL,'6985623147');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,13 +127,9 @@ CREATE TABLE `user_roles` (
 
 LOCK TABLES `user_roles` WRITE;
 /*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
-INSERT INTO `user_roles` VALUES (1,1),(2,2),(3,2);
+INSERT INTO `user_roles` VALUES (1,1),(4,1),(5,1),(2,2),(3,2),(6,2);
 /*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'doctor_appointment'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -144,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-20 23:28:24
+-- Dump completed on 2023-09-22  3:18:40
